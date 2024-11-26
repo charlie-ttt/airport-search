1c5821245244fa4a2130e2177327a6c448c32164
Charlie <achanont@gmail.com>
2024-11-26 22:54:54 +0700

    add search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleButtonClick() {
+    console.log(searchTerm);
+  }
+
   return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <h1>Get Origin Countries of Flight Arriving at an Airport</h1>
+      <label>
+        Enter 3-character Airport Code:
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </label>
+      <div>
+        <button type="button" onClick={handleButtonClick}>
+          Search
+        </button>
+      </div>
+    </div>
   );
 }
 
