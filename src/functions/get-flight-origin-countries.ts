f46d23e0e48f85cb2dcfb1045a4849ab548ef7e6
Charlie <achanont@gmail.com>
2024-11-26 23:13:32 +0700

    add data formatting function

---
+++ src/functions/get-flight-origin-countries.ts
@@ -0,0 +1,37 @@
+import { fetchCompschedule } from "./fetch";
+
+// fetch API and convert to desired data format
+export async function getFlightOriginCountries(fromCountry: string) {
+  const { data, error } = await fetchCompschedule(fromCountry);
+  if (error) throw new Error("failed to fetch");
+
+  const countryListArr = data.airport.pluginData.schedule.arrivals.data.map(
+    ({ flight }) => flight.airport.origin.position.country.name
+  );
+  return countCountriesWithMap(countryListArr);
+}
+
+// format the output into CountryCountResult format: [ { Thailand: 3 }, { China: 2 }, { USA: 1 } ]
+function countCountriesWithMap(countries: string[]): CountryCountResult {
+  const countryMap = new Map();
+
+  for (const country of countries) {
+    if (countryMap.has(country)) {
+      countryMap.set(country, countryMap.get(country) + 1);
+    } else {
+      countryMap.set(country, 1);
+    }
+  }
+
+  // format the data into normal js array
+  const result = [];
+  for (const [country, count] of countryMap) {
+    result.push({ [country]: count });
+  }
+
+  return result;
+}
+
+export type CountryCountResult = {
+  [x: string]: number;
+}[];
