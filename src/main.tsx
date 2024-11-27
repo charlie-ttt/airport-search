649506a9d750d38668fe421e71eb09237d3472b2
Charlie <achanont@gmail.com>
2024-11-27 08:45:07 +0700

    reorganize pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Airport from "./pages/Airport.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/twitter`}>Twitter</a>
+            </li>
+            <li>
+              <a href={`/airport`}>Airport</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: "/airport",
+    element: <Airport />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Twitter.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import type { CountryCountResult } from "../functions/get-flight-origin-countries";
+import { getFlightOriginCountries } from "../functions/get-flight-origin-countries";
+import "./AirportApp.css";
+
+function AirportApp() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState<CountryCountResult>([]); // format [ { Thailand: 3 }, { China: 2 } ]
+
+  async function handleButtonClick() {
+    setIsLoading(true);
+
+    const { data, error } = await getFlightOriginCountries(searchTerm);
+    if (error) {
+      setIsLoading(false);
+      alert(error);
+      return;
+    }
+
+    setResult(data);
+    setIsLoading(false);
+  }
+
+  return (
+    <div>
+      <h1>Get Origin Countries of Flight Arriving at an Airport</h1>
+      <label>
+        Enter 3-character Airport Code:
+        <br />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="button" onClick={handleButtonClick} disabled={isLoading}>
+        {isLoading ? "... is loading" : "Search"}
+      </button>
+      <div>
+        {result.length > 0 && (
+          <table className="w-full border-collapse">
+            <thead>
+              <tr>
+                <th className="p-2 border text-left">Country</th>
+                <th className="p-2 border text-right"># of Flights</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((item, index) => {
+                const country = Object.keys(item)[0];
+                const number = item[country];
+
+                return (
+                  <tr key={index}>
+                    <td className="p-2 border">{country}</td>
+                    <td className="p-2 border text-right">{number}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AirportApp;
